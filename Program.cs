2af3fb8029881ac1a997265b7f29efbf9393b3c4
Suraj Rajendra Pardeshi <152958796+SurajPardeshi636@users.noreply.github.com>
2023-12-11 14:54:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int i = 0;
+                while (i <= 5)
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
+                Console.ReadLine();
+
+
+            int j = 0;
+            do
+            {
+                Console.WriteLine(j);
+
+                j++;
+
+            }
+            while (j < 5);
+            Console.ReadLine();
+        }
+    }
+}
